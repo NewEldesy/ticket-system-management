ae03197bdb26c2c7372dff48461717f8c73147d5
Ramel Offrey Eldesy MAKOSSO <115371511+NewEldesy@users.noreply.github.com>
2025-02-23 14:40:19 +0000

    Create db.php

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'admin';
+$dbname = 'ticket_system';
+$username = 'Admin1234!';
+$password = '';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
